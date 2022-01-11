1770b6e572154f37cd9021626121ddabf311e8be
Jonathan Gonzalez <jonathangonzalez@JDG-Hearthstone.attlocal.net>
2022-01-10 21:53:36 -0500

    updated name and testing hot reload

--- amipod/lib/main.dart
+++ amipod/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Amipod',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Amipod Home Page'),
     );
   }
 }
